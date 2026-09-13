e525238cae3cdea4e1947ad9970fbb370b4932c0
Huaibrew <huaiuniversecc@gmail.com>
2026-09-13 17:29:18 +0800

    fix the screen not displaying

--- RemoteControl/RemoteControl.cpp
+++ RemoteControl/RemoteControl.cpp
@@ -1,5 +1,7 @@
 ﻿#include <stdio.h>
 #include <Windows.h>
+#include <atlimage.h>
+
 
 #pragma comment(lib, "ws2_32.lib")
 
@@ -9,12 +11,18 @@ struct Packet_Header {
     int cmd;
     int body_len; // by byte
 };
-
 #pragma pack(pop)
 
 struct Packet {
     Packet_Header header;
     char body[];
+};
+
+enum Cmd {
+    CMD_SCREEN = 1,
+    CMD_MOUSE = 2,
+    CMD_KEYBOARD = 4,
+    CMD_TESTCONNECT = 2026
 };
 
 #define RECV_BUFFER_SIZE 1024*1024*1
@@ -24,16 +32,27 @@ Packet* pack_packet(int cmd, char* buffer, int buffer_len);
 Packet* parse_packet(char* buffer, int len);
 int init_server(void);
 
+int handle_command(Packet* packet);
+int handle_screen(Packet* packet);
+int handle_mouse(Packet* packet);
+int handle_keyboard(Packet* packet);
+int handle_testconnect(Packet* packet);
+
 SOCKET g_server_socket = INVALID_SOCKET;
+SOCKET g_client_socket = INVALID_SOCKET;
 
 int main() {
-    init_server();
+#if 1    
+    if (init_server() != 0) {
+        printf("启动服务失败\r\n");
+        return 0;
+    }
     // 5）等待客户端连接
     SOCKADDR_IN client_addr;
     int client_addr_len = sizeof(SOCKADDR_IN);
     printf("SERVER\n");
     printf("等待客户端连接\r\n");
-    SOCKET client_socket = accept(g_server_socket, (sockaddr *)&client_addr, &client_addr_len);	// 阻塞
+    g_client_socket = accept(g_server_socket, (sockaddr*)&client_addr, &client_addr_len);    // 阻塞
     printf("客户端连接成功\r\n");
 
     // 6）等待接受客户端socket发送的数据
@@ -42,40 +61,139 @@ int main() {
 
     static int index = 0;
     while (true) {
-	    int len = recv(client_socket, buffer + index, RECV_BUFFER_SIZE - index, 0); // index : rest data size
-	    index += len; // buffer[packet1packet2pack0000000000000..00]  !!packet1 : packet:1\0
-	    Packet* packet = parse_packet(buffer, len); // parse one packet, rest move to buffer head
-	    index -= get_packet_len(packet); // how many char(byte) rest data
-	    memmove(buffer, buffer + get_packet_len(packet), index); // rest data move to buffer head
-    #if 0
-	    fwrite(buffer, sizeof(char), len, stdout);	// 换种打印方式
-	    fwrite("\r\n----\r\n", 1, 8, stdout);
-    #endif
-	    printf("server recv packet->body : %s\r\n", packet->body);
-	    printf("server recv packet->header.magic : %x\r\n", packet->header.magic);
-	    printf("server recv packet->header.cmd : %d\r\n", packet->header.cmd);
-	    printf("server recv packet->header.body_len : %d\r\n", packet->header.body_len);
+        int len = recv(g_client_socket, buffer + index, RECV_BUFFER_SIZE - index, 0); // index : rest data size
+        if (len > 0) {
+            index += len;                                                               // buffer[packet1packet2pack0000000000000..00]  !!packet1 : packet:1\0
+            printf("接受数据成功：%d\r\n", len);
+        }
+        else {
+            printf("接受数据失败\r\n");
+        }
 
-	    // 7）给客户端发送数据
-	    Packet *send_packet = pack_packet(packet->header.cmd, packet->body, packet->header.body_len);
-	    send(client_socket, (char *)&packet->header.magic, get_packet_len(send_packet), 0);
-	    printf("server send data : %s\r\n", &buffer[12]);
-
-	    free(packet);
-	    // Sleep(500);	// 模拟服务器处理命令（耗时1s）
+        if (index > 0) {
+            printf("开始解析数据\r\n");
+            Packet* packet = parse_packet(buffer, index);                                 // parse one packet, rest move to buffer head
+            if (packet == NULL) {
+                printf("解析数据失败\r\n");
+            }
+            printf("解析数据成功\r\n");
+            if (packet != NULL) {
+                index -= get_packet_len(packet);                                            // how many char(byte) rest data
+                memmove(buffer, buffer + get_packet_len(packet), index);                    // rest data move to buffer head
+                handle_command(packet);
+                free(packet);
+            }
+        }
     }
-	
+
     // 8）程序结束关闭套接字
-    closesocket(client_socket);
+    closesocket(g_client_socket);
     closesocket(g_server_socket);
     // 清除掉
     WSACleanup();
+#endif
+    handle_screen(NULL);
     return 0;
 }
 
+int handle_command(Packet* packet) {
+    printf("handle_command: cmd = %d\r\n", packet->header.cmd);
+    int ret = 0;
+    switch (packet->header.cmd) {
+    case CMD_SCREEN: {
+        ret = handle_screen(packet);
+        break;
+    }
+    case CMD_MOUSE: {
+        ret = handle_mouse(packet);
+        break;
+    }
+    case CMD_KEYBOARD: {
+        ret = handle_keyboard(packet);
+        break;
+    }
+    case CMD_TESTCONNECT: {
+        ret = handle_testconnect(packet);
+        break;
+    }
+    default:
+        break;
+    }
+    return ret;
+}
+
+
+int handle_screen(Packet* packet) {
+    CImage image; //  1）创建image对象
+    HDC h_screen = GetDC(NULL); // 2）拿到屏幕上下文
+    int bit_width = GetDeviceCaps(h_screen, BITSPIXEL); // 3）拿到屏幕像素位宽
+    printf("bit_width : %d\r\n", bit_width); // 32
+
+    SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2); // 让程序感知DPI缩放，获取真实屏幕分辨率
+
+    // 4）拿到屏幕宽高（以像素为单位）
+    int screen_width = GetSystemMetrics(SM_CXSCREEN);
+    int screen_height = GetSystemMetrics(SM_CYSCREEN);
+    printf("width : %d, height : %d\r\n", screen_width, screen_height);
+    image.Create(screen_width, screen_height, bit_width);
+
+    // 5）把屏幕数据复制到image的HDC里
+    BitBlt(image.GetDC(), 0, 0, screen_width, screen_height, h_screen, 0, 0,
+        SRCCOPY);
+    ReleaseDC(NULL, h_screen);
+    // image.Save("test.png", ::Gdiplus::ImageFormatPNG);
+
+    HGLOBAL heap_memory = GlobalAlloc(GMEM_MOVEABLE, 0);
+    if (heap_memory == NULL) return -1;
+
+    IStream* p_stream = NULL;
+    HRESULT ret = CreateStreamOnHGlobal(heap_memory, true, &p_stream);
+    if (ret == S_OK) {
+        // PNG -> [memory]
+        image.Save(p_stream, ::Gdiplus::ImageFormatPNG);
+        LARGE_INTEGER large_integer = { 0 };
+        p_stream->Seek(large_integer, STREAM_SEEK_SET, NULL); // p_stream -> heap_memory
+        char* pdata = (char*)GlobalLock(heap_memory);
+        int len = GlobalSize(heap_memory);
+
+        // 发送数据
+        Packet* packet = pack_packet(CMD_SCREEN, pdata, len);
+        int send_len = send(g_client_socket, (char*)packet, sizeof(Packet_Header) + len, 0);
+        if (send_len > 0) {
+            printf("发送数据成功 : %d\r\n", send_len);
+        }
+        else if (send_len < 0) {
+            printf("发送数据失败 : %d\r\n", send_len);
+        }
+        free(packet);
+        GlobalUnlock(heap_memory);
+    }
+
+    // cleanup
+    p_stream->Release();
+    GlobalFree(heap_memory);
+    image.ReleaseDC();
+    // 6）通过网络发送数据
+
+    return 0;
+}
+
+int handle_mouse(Packet* packet) {
+    return 0;
+}
+
+int handle_keyboard(Packet* packet) {
+    return 0;
+}
+
+int handle_testconnect(Packet* packet) {
+    return 0;
+}
+
+
 int get_packet_len(Packet* packet) {
     if (packet != NULL) {
-	    return packet->header.body_len + sizeof(packet->header);
+        return packet->header.body_len + sizeof(packet->header);
     }
     else return 0;
 }
@@ -85,39 +203,49 @@ Packet* parse_packet(char* buffer, int len) {
     // check header
     Packet packet;
     Packet* ppacket = NULL;
+
     int i = 0;
     for (; i < len; i += 4) {
-	if (*(int*)(buffer + i) == 0x55AA77CC) {
-	    packet.header.magic = *(int*)(buffer + i);
-	    i += 4;
-	    break;
-	}
+        if (*(int*)(buffer + i) == 0x55AA77CC) {
+            packet.header.magic = *(int*)(buffer + i);
+            i += 4;
+            break;
+        }
     }
-    /*
-        if ( *(int *)(buffer + i) == 0x55AA77CC) {
-        packet.header.magic = 0x55AA77CC;
-        i+=4;
-        } else return NULL;
-    */
+
     packet.header.cmd = *(int*)(buffer + i); i += 4;
     packet.header.body_len = *(int*)(buffer + i); i += 4; // follow body data
     if (packet.header.body_len > 0) {
-	    ppacket = (Packet*)malloc(sizeof(Packet) + packet.header.body_len);
-	    memcpy(ppacket->body, (char *)(buffer + i), packet.header.body_len);
-	    memcpy(&ppacket->header, &packet.header, sizeof(ppacket->header));
-	    return ppacket;
+        ppacket = (Packet*)malloc(sizeof(Packet) + packet.header.body_len);
+        if (ppacket == NULL)
+            return NULL;
+
+        memcpy(ppacket->body, (char*)(buffer + i), packet.header.body_len);
+        memcpy(&ppacket->header, &packet.header, sizeof(ppacket->header));
+        return ppacket;
     }
+    if (packet.header.body_len == 0) {
+        ppacket = (Packet*)malloc(sizeof(Packet));
+        if (ppacket == NULL)
+            return NULL;
+
+        memcpy(&ppacket->header, &packet.header, sizeof(ppacket->header));
+        return ppacket;
+    }
+    return NULL;
 }
 
 Packet* pack_packet(int cmd, char* buffer, int buffer_len) {
-    Packet* packet = (Packet*)malloc(buffer_len + sizeof(packet->header));
+    Packet* packet = (Packet*)malloc(buffer_len + sizeof(Packet_Header));
+    if (packet == NULL)
+        return NULL;
+
     packet->header.magic = 0x55AA77CC;
     packet->header.cmd = cmd;
     packet->header.body_len = buffer_len;
     if (buffer && buffer_len > 0) {
-        memcpy(packet->body, buffer, buffer_len);   
+        memcpy(packet->body, buffer, buffer_len);
     }
-    else return NULL;
     return packet;
 }
 
@@ -132,24 +260,26 @@ int init_server(void) {
     g_server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
     if (g_server_socket == INVALID_SOCKET) {
         printf("创建服务器socket失败\r\n");
-        return 0;
+        return -1;
     }
 
     // 3）给服务器绑定地址
-    SOCKADDR_IN server_addr;				// 准备一个地址(ip+port)
-    server_addr.sin_family = AF_INET;		// ipv4协议
-    server_addr.sin_port = htons(9999);		// 监听端口
-    server_addr.sin_addr.S_un.S_addr = inet_addr("192.168.1.4");	// 监听服务器所有ip，因为电脑不止一个ip地址
+    SOCKADDR_IN server_addr;                // 准备一个地址(ip+port)
+    server_addr.sin_family = AF_INET;        // ipv4协议
+    server_addr.sin_port = htons(9999);        // 监听端口
+    server_addr.sin_addr.S_un.S_addr = inet_addr("192.168.1.4");    // 监听服务器所有ip，因为电脑不止一个ip地址
     rc = bind(g_server_socket, (sockaddr*)&server_addr, sizeof(SOCKADDR_IN));
     if (rc == SOCKET_ERROR) {
         printf("Failed to bind server_addr!\r\n");
-        return 0;
+        return -2;
     }
 
     // 4）开启服务器socket监听, backlog：允许完成三次握手的客户端数量
     rc = listen(g_server_socket, 1);
     if (rc == SOCKET_ERROR) {
         printf("开启监听失败\r\n");
-        return 0;
+        return -3;
     }
+
+    return 0;
 }
--- client/client.cpp
+++ client/client.cpp
@@ -1,8 +1,11 @@
 ﻿#include <stdio.h>
 #include <Windows.h>
+#include <atlimage.h>
 
 #pragma comment(lib, "ws2_32.lib")
+#define RECV_BUFFER_SIZE 1024*1024*10
 
+// data struct
 #pragma pack(push, 1)
 struct Packet_Header {
     int magic;
@@ -16,88 +19,257 @@ struct Packet {
     char body[];
 };
 
-#define RECV_BUFFER_SIZE 1024*1024*1
+enum Cmd {
+    CMD_SCREEN = 1,
+    CMD_MOUSE = 2,
+    CMD_KEYBOARD = 4,
+    CMD_TESTCONNECT = 2026
+};
 
+// function prototype
 Packet* pack_packet(int cmd, char* buffer, int buffer_len);
 Packet* parse_packet(char* buffer, int len);
-int init_scoket(void);
+int init_socket(void);
+int get_packet_len(Packet* packet);
+int init_window(HINSTANCE handle_instance, int num_cmd_show);
 
+DWORD WINAPI send_screen_callback(LPVOID lpThreadParameter);
+
+// global variables
 SOCKET g_client_socket;
 SOCKADDR_IN g_server_addr;
+HWND g_hwnd = NULL;
+CImage g_image;
+CRITICAL_SECTION g_critical_section; // used to protect g_image
 
-int main (void) {
-    printf("CLIENT\n");
-    int rc;
+// typedef LRESULT (CALLBACK* WNDPROC)(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK winProc(HWND hwnd, UINT msg, WPARAM w_param, LPARAM l_param) {
+    switch (msg) {
+    case WM_PAINT: {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+        // got image
+        if (!g_image.IsNull()) {
+            RECT client_rect;
+            GetClientRect(hwnd, &client_rect);
+            int client_width = client_rect.right - client_rect.left;
+            int client_height = client_rect.bottom - client_rect.top;
 
-    init_scoket();
+            int old_mode = SetStretchBltMode(hdc, HALFTONE); // HALFTONE 高清
+            SetBrushOrgEx(hdc, 0, 0, NULL); // set brush origin location
+            
+            EnterCriticalSection(&g_critical_section); // enter critical section to protect g_image
+            int remote_width = g_image.GetWidth();
+            int remote_height = g_image.GetHeight();
 
-    // 3）连接服务器
-    rc = connect(g_client_socket, (sockaddr *)&g_server_addr, sizeof(SOCKADDR_IN));
-    if (rc == SOCKET_ERROR) {
-	printf("连接服务器失败\r\n");
-	return 0;
+            g_image.StretchBlt(hdc, 0, 0, client_width, client_height, 0, 0,
+                remote_width, remote_height, SRCCOPY);
+            LeaveCriticalSection(&g_critical_section); // leave critical section
+
+            SetStretchBltMode(hdc, old_mode);
+        }
+
+        EndPaint(hwnd, &ps);
+        break;
     }
-    printf("连接服务器成功\r\n");
-
-    // 4）向服务端发送数据
-    char buffer[1024] = {0};
-
-    char *recv_buffer = (char *)malloc(RECV_BUFFER_SIZE);
-    int count = 0;
-
-    while (true) {
-	count++;
-	printf("请输入要发送的数据: ");	// 接受用户输入 stdin
-	fgets(buffer, 1024, stdin);
-		
-	//snprintf(buffer, 1024, "packet:%d", count);
-
-	//Packet* packet = (Packet*)malloc(sizeof(Packet) + 10);
-	//packet->header.magic = 0x55AA77CC;
-	//packet->header.cmd = 2000;
-	//packet->header.body_len = 10;
-	//memcpy(packet->body, buffer, 10);
-		
-	Packet *send_packet = pack_packet(2000, buffer, 10);
-	// start address and data size : "<header> + packet:1"
-	send(g_client_socket,
-	     (char*)&send_packet->header.magic, 
-	     send_packet->header.body_len + sizeof(send_packet->header), 
-	     0); // Can (char *)packet?
-	free(send_packet);
-	printf("client send data: %s\r\n", buffer);
-
-	// 5）客户端等待接收服务端数据
-	int len = recv(g_client_socket, recv_buffer, RECV_BUFFER_SIZE, 0);
-	if (len > 0) {
-	    Packet* recv_packet = parse_packet(recv_buffer, len);
-	    printf("client recv data : %s\r\n", recv_packet->body);
-	    printf("client recv packet->header.magic : %x\r\n", recv_packet->header.magic);
-	    printf("client recv packet->header.cmd : %d\r\n", recv_packet->header.cmd);
-	    printf("client recv packet->header.body_len : %d\r\n", recv_packet->header.body_len);
-	    free(recv_packet);
-	}
-		
-	// Sleep(10);
+    default:
+        return DefWindowProc(hwnd, msg, w_param, l_param);
+        break;
     }
-
-
-    // 6）程序结束关闭套接字
-    closesocket(g_client_socket);
-    // 清除
-    WSACleanup();
     return 0;
 }
 
+// entry point of windows GUI program
+int WINAPI WinMain(HINSTANCE handle_instance,
+    HINSTANCE handle_previous_instance, PSTR p_cmd_line,
+    int num_cmd_show)
+{
+    InitializeCriticalSection(&g_critical_section);
+    init_window(handle_instance, num_cmd_show);
+    init_socket();
+
+    // connect server
+    int rc = connect(g_client_socket, (sockaddr*)&g_server_addr,
+        sizeof(SOCKADDR_IN));
+    if (rc == SOCKET_ERROR) {
+        printf("连接服务器失败\r\n");
+        return 0;
+    }
+
+    // create thread used to loop send screen data
+    DWORD send_screen_thread_id = 0;
+    HANDLE handle_send_screen = CreateThread(NULL, 0, send_screen_callback, NULL, 0, &send_screen_thread_id);
+    OutputDebugString("连接服务器成功\r\n");
+
+    // process message like queue
+    MSG message = { 0 };
+    while (GetMessage(&message, NULL, 0, 0)) {
+        TranslateMessage(&message);
+        DispatchMessage(&message);
+    }
+}
+
+DWORD WINAPI send_screen_callback(LPVOID lpThreadParameter) {
+    char* receive_buffer = (char*)malloc(RECV_BUFFER_SIZE);
+    if (receive_buffer == NULL) {
+        printf("malloc receive_buffer failed\r\n");
+        return 0;
+    }
+    
+    while (true) {
+        Packet* packet = pack_packet(CMD_SCREEN, NULL, 0); // just send one cammand
+        int send_len = send(g_client_socket, (char*)&packet->header.magic, get_packet_len(packet), 0);
+        if (send_len > 0) {
+            OutputDebugString("成功发送数据");
+        }
+        free(packet);
+        int len = recv(g_client_socket, receive_buffer, RECV_BUFFER_SIZE, 0);
+        if (len > 0) {
+            Packet* receive_packet = parse_packet(receive_buffer, RECV_BUFFER_SIZE);
+            if (receive_packet != NULL) { // parsed packet successful
+                // process packet body data
+                HGLOBAL heap_memory = GlobalAlloc(GMEM_MOVEABLE, 0);
+                if (heap_memory == NULL)
+                    continue;
+
+                IStream* p_stream = NULL;
+                HRESULT ret = CreateStreamOnHGlobal(heap_memory, true, &p_stream);
+                if (ret == S_OK) {
+                    ULONG length = 0;
+                    p_stream->Write(receive_packet->body, receive_packet->header.body_len, &length);
+                    free(receive_packet); // free receive_packet to avoid memory leak
+
+                    LARGE_INTEGER large_integer = { 0 };
+                    p_stream->Seek(large_integer, STREAM_SEEK_SET, NULL); // set stream pointer to the beginning of the stream
+                    
+                    EnterCriticalSection(&g_critical_section); // enter critical section to protect g_image
+                    if (g_image.IsNull() == FALSE) {
+                        g_image.Destroy(); // destroy the old image to avoid memory leak
+                    }
+                    g_image.Load(p_stream); // load image from stream
+                    LeaveCriticalSection(&g_critical_section); // leave critical section
+
+                    InvalidateRect(g_hwnd, NULL, FALSE); // invalidate the entire client area of the window, causing a WM_PAINT message to be sent to the window
+                    UpdateWindow(g_hwnd); // force the window to be repainted immediately, causing a WM_PAINT message to be sent to the window
+                }
+            }
+        }
+    }
+}
+
+int init_window(HINSTANCE handle_instance, int num_cmd_show) {
+    // register window class
+    WNDCLASS window_class = { 0 };
+    LPCSTR class_name = "MainWindow";
+    window_class.lpfnWndProc = winProc;
+    window_class.hInstance = handle_instance;
+    window_class.lpszClassName = class_name;
+    window_class.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    window_class.hCursor = LoadCursorA(NULL, IDC_ARROW);
+    window_class.hIcon = LoadIconA(NULL, IDI_APPLICATION);
+    window_class.style = CS_HREDRAW | CS_VREDRAW;
+
+    if (!RegisterClass(&window_class)) {
+        MessageBox(NULL, "register window failed!", "error",
+            MB_OK | MB_ICONERROR);
+        return 0;
+    }
+
+    // create window
+    g_hwnd = CreateWindow(class_name, "Remote Control", WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 600, 400, NULL, NULL,
+        handle_instance, NULL);
+    if (g_hwnd == NULL) {
+        MessageBox(NULL, "create window failed!", "error", MB_OK | MB_ICONERROR);
+        return 0;
+    }
+
+    ShowWindow(g_hwnd, num_cmd_show);
+
+    UpdateWindow(g_hwnd);
+}
+
+/* main just a console program, not a windows GUI program, so no need to use WinMain */
+//int main(void) {
+//    printf("CLIENT\n");
+//    init_scoket();
+//
+//    // 3）连接服务器
+//    if (connect(g_client_socket, (sockaddr*)&g_server_addr, sizeof(SOCKADDR_IN)) == SOCKET_ERROR) {
+//        printf("连接服务器失败\r\n");
+//        return 0;
+//    }
+//    printf("连接服务器成功\r\n");
+//
+//    // 4）向服务端发送数据
+//    char buffer[1024] = { 0 };
+//
+//    char* recv_buffer = (char*)malloc(RECV_BUFFER_SIZE);
+//    if (recv_buffer == NULL) {
+//        printf("malloc recv_buffer failed\r\n");
+//        return 0;
+//    }
+//    int count = 0;
+//
+//    while (true) {
+//        count++;
+//        printf("请输入要发送的数据: ");    // 接受用户输入 stdin
+//        fgets(buffer, 1024, stdin);
+//
+//        //snprintf(buffer, 1024, "packet:%d", count);
+//
+//        //Packet* packet = (Packet*)malloc(sizeof(Packet) + 10);
+//        //packet->header.magic = 0x55AA77CC;
+//        //packet->header.cmd = 2000;
+//        //packet->header.body_len = 10;
+//        //memcpy(packet->body, buffer, 10);
+//
+//        Packet* send_packet = pack_packet(1, buffer, 10);
+//        // start address and data size : "<header> + packet:1"
+//        send(g_client_socket,
+//            (char*)&send_packet->header.magic,
+//            send_packet->header.body_len + sizeof(send_packet->header),
+//            0); // Can (char *)packet?
+//        free(send_packet);
+//        printf("client send data: %s\r\n", buffer);
+//
+//        // 5）客户端等待接收服务端数据
+//        int len = recv(g_client_socket, recv_buffer, RECV_BUFFER_SIZE, 0);
+//        if (len > 0) {
+//            Packet* recv_packet = parse_packet(recv_buffer, len);
+//            printf("client recv data : %s\r\n", recv_packet->body);
+//            printf("client recv packet->header.magic : %x\r\n", recv_packet->header.magic);
+//            printf("client recv packet->header.cmd : %d\r\n", recv_packet->header.cmd);
+//            printf("client recv packet->header.body_len : %d\r\n", recv_packet->header.body_len);
+//            if (recv_packet->header.cmd == 1) {
+//                // 服务器返回了屏幕数据
+//                // 解析屏幕数据
+//            }
+//            free(recv_packet);
+//        }
+//
+//        // Sleep(10);
+//    }
+//
+//
+//    // 6）程序结束关闭套接字
+//    closesocket(g_client_socket);
+//    // 清除
+//    WSACleanup();
+//    return 0;
+//}
+
 Packet* pack_packet(int cmd, char* buffer, int buffer_len) {
-    Packet* packet = (Packet*)malloc(buffer_len + sizeof(packet->header));
+    Packet* packet = (Packet*)malloc(buffer_len + sizeof(Packet_Header));
+    if (packet == NULL) {
+        return NULL;
+    }
     packet->header.magic = 0x55AA77CC;
     packet->header.cmd = cmd;
     packet->header.body_len = buffer_len;
     if (buffer && buffer_len > 0) {
-	memcpy(packet->body, buffer, buffer_len);
+        memcpy(packet->body, buffer, buffer_len);
     }
-    else return NULL;
     return packet;
 }
 
@@ -107,11 +279,11 @@ Packet* parse_packet(char* buffer, int len) {
     Packet* ppacket = NULL;
     int i = 0;
     for (; i < len; i += 4) {
-	if (*(int*)(buffer + i) == 0x55AA77CC) {
-	    packet.header.magic = *(int*)(buffer + i);
-	    i += 4;
-	    break;
-	}
+        if (*(int*)(buffer + i) == 0x55AA77CC) {
+            packet.header.magic = *(int*)(buffer + i);
+            i += 4;
+            break;
+        }
     }
     /*
       if ( *(int *)(buffer + i) == 0x55AA77CC) {
@@ -122,15 +294,26 @@ Packet* parse_packet(char* buffer, int len) {
     packet.header.cmd = *(int*)(buffer + i); i += 4;
     packet.header.body_len = *(int*)(buffer + i); i += 4; // follow body data
     if (packet.header.body_len > 0) {
-	ppacket = (Packet*)malloc(sizeof(Packet) + packet.header.body_len);
-	memcpy(ppacket->body, (char*)(buffer + i), packet.header.body_len);
-	memcpy(&ppacket->header, &packet.header, sizeof(ppacket->header));
-	return ppacket;
+        ppacket = (Packet*)malloc(sizeof(Packet) + packet.header.body_len);
+        if (ppacket == NULL)
+            return NULL;
+
+        memcpy(ppacket->body, (char*)(buffer + i), packet.header.body_len);
+        memcpy(&ppacket->header, &packet.header, sizeof(ppacket->header));
+        return ppacket;
+    }
+    if (packet.header.body_len == 0) {
+        ppacket = (Packet*)malloc(sizeof(Packet));
+        if (ppacket == NULL)
+            return NULL;
+
+        memcpy(&ppacket->header, &packet.header, sizeof(ppacket->header));
+        return ppacket;
     }
     return NULL;
 }
 
-int init_scoket(void) {
+int init_socket(void) {
     // 客户端网络编程步骤
     // 1）初始化网络环境
     WSADATA wsadata;
@@ -144,6 +327,14 @@ int init_scoket(void) {
     }
     g_server_addr;
     g_server_addr.sin_family = AF_INET;
-    g_server_addr.sin_port = htons(9999);	// 转为网络字节序
+    g_server_addr.sin_port = htons(9999);    // 转为网络字节序
     g_server_addr.sin_addr.S_un.S_addr = inet_addr("192.168.1.4");
+}
+
+int get_packet_len(Packet* packet) {
+    if (packet != NULL) {
+        return packet->header.body_len + sizeof(packet->header);
+    }
+    else
+        return 0;
 }
